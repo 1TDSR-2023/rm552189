a085aefd5f60b2f01affaa9a023603faadd63645
matdelgado <matdelgado0@gmail.com>
2023-04-28 23:02:36 -0300

    Aula JavaScript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var nome1 = "Petroni";
+let nome2 = "Derick"; //let respeita o escopo
+const nome3 = "Ruan"; //Constante (imutavel)
+
+// const PI = 3.14;
+
+if(nome1 != ""){
+    let nome1 = "Geovana";
+}
