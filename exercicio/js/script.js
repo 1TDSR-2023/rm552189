73ede2eba0a2bf0ad426bb67e25c794b399a2ed0
matdelgado <matdelgado0@gmail.com>
2023-05-05 22:13:37 -0300

    Atualizando led

---
+++ exercicio/js/script.js
@@ -0,0 +1,83 @@
+// var nome1 = "Petroni";
+// // let nome2 = "Derick"; //let respeita o escopo
+// // const nome3 = "Ruan"; //Constante (imutavel)
+
+// // const PI = 3.14;
+
+// if(nome1 != ""){
+//     let nome1 = "Geovana";
+// }
+
+let botao = document.getElementById("meu-btn"); //captura o botão
+// console.log(botao.textContent);
+
+botao.addEventListener("click", function(){
+    // console.log(this.textContent);
+    //Função matemática Math
+    //random = retorna número aleatório entre 0 e 1 ex: 0.2412425;
+    //floor = arredonda um número para baixo.
+    //ceil = arredonda um número para cima.
+    //round = arredonda um número aleatoriamente.
+
+    let r=0, g=0, b=0;
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+
+    // this.setAttribute("style", "background-color:rgb("+r+","+g+","+b+");");
+    this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
+
+});
+
+//ESTUDAR
+//querySelector
+//querySelectorAll
+//Array
+//Estruturas de repetição
+
+//Declarar uma Array
+
+let frutas = ["banana", "maçã", "pêssego", "laranja", "limão", "melancia", "kiwi"]
+
+//Imprimindo o array
+
+// console.log(frutas);
+// console.table(frutas[0]);
+
+// //Inserir um item ao final do array com método push(nomeDoItem);
+
+// frutas.push("Manga");
+// console.log(frutas)
+
+// //Inserir item no início do array com método unshift(nomeDoItem);
+
+// frutas.unshift("pitaya");
+// console.log(frutas);
+
+// //remover item no final do array com método pop();
+
+// frutas.pop();
+// console.log(frutas);
+
+// //remover item no início do array com método pop();
+
+// frutas.shift();
+// console.log(frutas);
+
+// //Localizar um item no array utilizando o método indexOf(nomeDoItem);
+// //Retorna o indíce do objeto
+
+// let indice = frutas.indexOf("laranja");
+// console.log(`o índice encontrado foi : ${indice}`);
+// console.log(`o item buscado foi : ${frutas[indice]}`);
+
+//Realizando uma remoção de item utilizando o método splice().
+//o método splacei() recebe dois parâmetros, indíce do item que será removido e a quantidade de vezes que este mesmo índice será removido. Ex:splice(indice, 1);
+//Dica: utilize o método indexOf() para buscar o índice do item no array.
+
+// let indice = frutas.indexOf("melancia");
+// console.log(`O item encontrado no índice antes da remoção foi: ${frutas[indice]}`);
+// frutas.splice(indice, 1);
+// console.log(frutas);
+// console.log(`O item encontrado no índice depois da remoção foi ${frutas[indice]}`);
+
--- exercicio1/js/exercicio1.js
+++ exercicio1/js/exercicio1.js
@@ -13,12 +13,27 @@ tela.addEventListener("click", function(){
     botao.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
 
 })
+//Questão 2
+let botao_formulario = document.getElementById("ReceberInfo");
+
+botao_formulario.addEventListener("click", function(){
+   let email =  document.querySelector("#IdEmail");
+   let lista =  document.querySelector("#Lista");
+   let texto = email.value;
+   let newItem = document.createElement('li');
+   newItem.innerText = texto;
+
+   lista.appendChild(newItem);
+
+});
+
+
 //Questão 3
 let botao_imagem = document.getElementById("btn-imagem");
 
 botao_imagem.addEventListener("click", function(){
     let img = document.querySelector("#imagem_lobo")
-    img.setAttribute('src', '../img/lobo2.jpg');
+    img.setAttribute('src', '../exercicio/img/lobo2.jpg');
 
 })
 
@@ -29,3 +44,12 @@ botao_html.addEventListener("click", function(){
     let mudar = document.querySelector("#variavel_p");
     mudar.textContent = "Novo texto!";
 })
+
+//Questão 5
+let botao_ocultar = document.getElementById("btn-ocultar");
+
+botao_ocultar.addEventListener("click", function(){
+    let ocultar = document.querySelector("#variavel_p");
+    ocultar.style.display = 'none';
+    
+});
